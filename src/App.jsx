9681a517a66732d709082a51b2fc1413ec7aaec1
qlonix <qloopy@gmail.com>
2026-02-10 14:51:53 +0900

    release: v1.2.0 - scroll speed setting, Linux scroll improvements, Windows+Linux CI

--- src/App.jsx
+++ src/App.jsx
@@ -401,7 +401,7 @@ function App() {
             <h2>About YATS</h2>
             <p><strong>YATS</strong> stands for:</p>
             <p className="yats-full-name">Yet Another Touchpad Shortcut</p>
-            <p className="version-info">Version 1.1.0</p>
+            <p className="version-info">Version 1.2.0</p>
             <button className="btn-close-modal" onClick={() => setShowAbout(false)}>Close</button>
           </div>
         </div>
